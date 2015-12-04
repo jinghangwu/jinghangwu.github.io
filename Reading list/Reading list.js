306007e4f41cf11e7d714d63bcc943663af92320
jinghangwu <j-wu@kellogg.northwestern.edu>
2015-12-03 21:47:56 -0500

    Final project1

---
+++ Reading list/Reading list.js
@@ -0,0 +1,12 @@
+// This code runs when the page loads
+$(function() {
+
+  $(".thumbnail").on("click", function(event) {
+    event.preventDefault();
+    var elementThatWasClicked = $(this);
+
+    elementThatWasClicked.parent().remove(); 
+    //or $(this).remove()
+  })
+
+})
